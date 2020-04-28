377211e52915dc3765841ee70b4350a5679ff446
PrishaSusanne <62793055+PrishaSusanne@users.noreply.github.com>
2020-04-28 19:25:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,87 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, pig1;
+var backgroundImg,platform;
+var bird, slingShot;
+
+function preload() {
+    backgroundImg = loadImage("sprites/bg.png");
+}
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150, 305, 300, 170);
+
+    box1 = new Box(700,320,70,70);
+    box1.shapeColor = "red";
+    box2 = new Box(920,320,70,70);
+    box2.shapeColor = "blue";
+    pig1 = new Pig(810, 350);
+    pig1.shapeColor = "purple";
+    log1 = new Log(810,260,300, PI/2);
+    log1.shapeColor = "yellow";
+
+    box3 = new Box(700,240,70,70);
+    box3.shapeColor = "orange";
+    box4 = new Box(920,240,70,70);
+    box4.shapeColor = "blue";
+    pig3 = new Pig(810, 220);
+    pig3.shapeColor = "turquoise";
+
+    log3 =  new Log(810,180,300, PI/2);
+    log3.shapeColor = "red";
+
+    box5 = new Box(810,160,70,70);
+    box5.shapeColor = "pink";
+    log4 = new Log(760,120,150, PI/7);
+    log4.shapeColor = "red";
+    log5 = new Log(870,120,150, -PI/7);
+    box1.shapeColor = "turquoise";
+
+    bird = new Bird(100,100);
+    fill("turquoise");
+
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot = new SlingShot(bird.body,{x:200,y:100});
+}
+
+function draw(){
+    background(backgroundImg);
+    Engine.update(engine);
+    strokeWeight(4);
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    log1.display();
+
+    box3.display();
+    box4.display();
+    pig3.display();
+    log3.display();
+
+    box5.display();
+    log4.display();
+    log5.display();
+
+    bird.display();
+    platform.display();
+    slingshot.display();    
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY}); 
+}
+
+function mouseReleased(){
+    slingshot.fly();
+}
